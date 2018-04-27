53debb43ffa08dc04890e8e8fe8de47f9216eef0
Kyle <k.yasumiishi@gmail.com>
2018-04-27 16:16:14 -0700

    Created all the read-only pages for the site

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -18,6 +19,18 @@ AuthorSchema.virtual('name').get(function() {
 AuthorSchema.virtual('url').get(function() {
     return '/catalog/author/' + this._id;
 });
+
+AuthorSchema.virtual('birth_formatted').get(function() {
+    return (this.date_of_birth) ? moment(this.date_of_birth).format('YYYY-MM-DD') : 'unknown';
+});
+
+AuthorSchema.virtual('death_formatted').get(function() {
+    return (this.date_of_death) ? moment(this.date_of_death).format('YYYY-MM-DD') : 'present';
+});
+
+
+
+
 
 // Export model
 module.exports = mongoose.model('Author', AuthorSchema);
