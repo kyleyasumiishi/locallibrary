cfaf467f7a5bfa2ab9d7d6ec514daa12c7457fbd
Kyle <k.yasumiishi@gmail.com>
2018-04-27 17:45:15 -0700

    Left off at creating Author objects

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,8 @@
 var Author = require('../models/author');
 var async = require('async');
 var Book = require('../models/book');
-
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // Display list of all Authors.
 exports.author_list = function(req, res) {
@@ -40,13 +41,28 @@ exports.author_detail = function(req, res) {
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', { title: 'Create Author' });
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    // Validate fields.
+    body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified.').isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1 }).trim(),withMessage('Family name must be specified.').isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+    // Sanitize fields.
+
+    // Process request after validation and sanitization.
+
+        // Extract the validation errors from a request.
+
+            // Data from form is valid.
+
+            // Create an Author object with escapted and trimmed data.
+
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
