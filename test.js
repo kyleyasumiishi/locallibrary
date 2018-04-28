5418838341827e743c156665073c15376a94086a
Kyle <k.yasumiishi@gmail.com>
2018-04-27 20:26:45 -0700

    Added author and book forms and updated get/post methods for each. Left off at bookinstance form

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-console.log(global);
+var async = require('async');
+
+async.parallel([
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 'one');
+        }, 2000);
+    },
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 'two');
+        }, 1000);
+    }
+],
+// optional callback
+function(err, results) {
+    // the results array will equal ['one', 'two'] even though the second function that a shorter timeout.
+    if (err) return console.log(err);
+    return console.log(results);
+});
+
+
+console.log(([] instanceof Array));
